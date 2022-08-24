ae6c6c5d0eab447950d69b4575a8293ec13e1327
zmweske <31971632+zmweske@users.noreply.github.com>
2022-08-24 17:34:14 -0500

    sorts horizontal and vertical images

--- pull.bat
+++ pull.bat
@@ -24,10 +24,10 @@ for /r %%i in (*) do echo %%i >> ..\trash.txt
 REM CALL explorer.exe "%DIRECTORY%\lock screens\"
 
 for /f %%f in ('dir /b .') do @(
-	echo %%f
+	REM echo %%f
 	REM for /f "delims=? tokens=2" %%a in ('call %~dp0toolTipInfo.bat %%f ^|find "Dimensions:"')  do (
-	for /f "delims=" %%a in ('call %~dp0imgInfo.bat %%f')  do (
-		echo "%%a"
+	@for /f "delims=" %%a in ('call %~dp0imgInfo.bat %%f')  do (
+		REM echo %%a
 		@if "%%a" == "1920 x 1080" @move %%f ..\horizontal\
 		@if "%%a" == "1080 x 1920" @move %%f ..\vertical\
 	)
@@ -35,3 +35,4 @@ for /f %%f in ('dir /b .') do @(
 move "%DIRECTORY%\lock screens\temp\*" "%DIRECTORY%\lock screens\dump\"
 cd ..
 rmdir "%DIRECTORY%\lock screens\temp\"
+

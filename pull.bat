23bfe6c4ea57e60a9a435758dc2d61876e03c4ba
zmweske <zmweske@crimson.ua.edu>
2025-04-01 11:00:49 -0500

    cleaner control and sorting

--- pull.bat
+++ pull.bat
@@ -1,39 +1,37 @@
 : Written by zmweske
 @echo off
-REM setup location
-SET DIRECTORY="%USERPROFILE%\Pictures"
-SET LOCATION="Lock Screens\"
 
-cd "%DIRECTORY%"
-if not exist "%LOCATION%" 		mkdir "%LOCATION%"
-if exist "%~dp0\imgInfo.bat" 	move "%~dp0\imgInfo.bat" %LOCATION%
-
-cd "%LOCATION%"
+echo Creating/checking directories...
 if not exist "dump\" 			mkdir "dump\"
 if not exist "temp" 			mkdir "temp\"
 if not exist "horizontal\" 		mkdir "horizontal\"
 if not exist "vertical\" 		mkdir "vertical\"
 
+echo Copying and renaming files into jpeg...
 @copy "%AppData%\..\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets\*" ".\temp" >nul 2>nul
-cd "temp"
+cd "%~dp0\temp\"
 ren * *.jpeg
 
-REM Deleting known files
-for /f "delims=" %%f in (..\trash.txt) do @del "%%f" 2>nul
-REM adding processed files to trash.txt
-for /r %%i in (*) do echo %%i >> ..\trash.txt
+echo Deleting known files...
+for /f "delims=" %%f in ("%~dp0\trash.txt") do @del "%%f" 2>nul
 
+echo Recording processed files...
+for %%a in (*) do @echo %%a >> "%~dp0\trash.txt"
+
+echo Sorting horizontal and vertical pictures
 for /f %%f in ('dir /b .') do @(
-	REM echo %%f
-	REM for /f "delims=? tokens=2" %%a in ('call ..\toolTipInfo.bat %%f ^|find "Dimensions:"')  do (
-	@for /f "delims=" %%a in ('call ..\imgInfo.bat %%f')  do (
+	@for /f "delims=" %%a in ('call "%~dp0\imgInfo.bat" %%f')  do (
 		REM echo %%a
-		@if "%%a" == "1920 x 1080" @move %%f ..\horizontal\
-		@if "%%a" == "1080 x 1920" @move %%f ..\vertical\
+		@if "%%a" == "1920 x 1080" @move %%f "%~dp0\horizontal\"
+		@if "%%a" == "1080 x 1920" @move %%f "%~dp0\vertical\"
 	)
 )
-cd ..
-move "temp\*" "dump\"
-rmdir "temp\"
+
+echo Cleaning up...
+cd "%~dp0"
+rd "%~dp0\temp\"
+if exist "%~dp0\temp\"  move "%~dp0\temp\*" "%~dp0\dump\"
+if exist "%~dp0\temp\"  rmdir "%~dp0\temp\"
+
 REM CALL explorer.exe .
 
